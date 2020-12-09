bb455b37750db7fb90e507a37c6bbbcd1b7b7b2d
Chaitanya2308 <35360720+Chaitanya2308@users.noreply.github.com>
2020-12-09 10:59:06 +0530

    Update script.js

--- Week2/day2/script.js
+++ Week2/day2/script.js
@@ -4,7 +4,7 @@ request.open('GET','https://restcountries.eu/rest/v2/all', true)
 request.send();
 request.onload = function () {
     var data = JSON.parse(this.response);
-
+/*
  /* 1.*/    var asia_region=data.filter(item=>item.region==="Asia")
             console.log(asia_region)
   /* 2. */   var population=data.filter(item=>item.population<200000)
@@ -20,10 +20,22 @@ request.onload = function () {
              return total+b.population
             }, 0)
             console.log(total_population)
-/*
-  var currency=data.filter(element=>element.currencies[0].name="United States dollar");
-console.log(currency)
-*/
+/* 5. */
+var usd_country=data.filter((item)=>
+{
+let flag=false
+item.currencies.forEach(element=>
+{
+if(element.code==='USD')
+flag=true;});
+if(flag==true)
+return true;
+else
+return false;
+});
+console.log(usd_country);
+
+
 }
 
 
